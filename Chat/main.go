8f1653477c9edca6a8b1097cdfd66c6ada3b00fb
yu_naga24 <nagasaki.y24@gmail.com>
2023-02-04 19:57:10 +0900

    OAuth認証機能の追加

--- Chat/main.go
+++ Chat/main.go
@@ -10,6 +10,11 @@ import (
 	"text/template"
 
 	"github.com/YuNaga224/websocketChat/websocketChat/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 // templateHandler型を宣言
@@ -26,8 +31,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		//テンプレートをコンパイル
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
 
-	t.templ.Execute(w, r)
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 
 }
 
@@ -35,11 +47,21 @@ func main() {
 
 	var addres = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+
+	//Gomniauthセットアップ
+	gomniauth.SetSecurityKey("デジタル署名となるランダムな値")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localohost:8080/auth/callback/facebook"),
+		github.New("88201b7004c5b9ceab8f", "7f2a5c9bbfce6013f29453f6f32e08d93219e5cd", "http://localhost:8080/auth/callback/github"),
+		google.New("515360048655-070u177uq2879ol0jafs95g47ov1eiau.apps.googleusercontent.com", "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", "http://localhost:8080/auth/callback/google"),
+	)
 	//ROOMの新規作成
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	//ルート
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	//チャットルームの開始
 	go r.run()
